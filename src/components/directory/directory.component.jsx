6771b7bfcad35d267678ad3ea54b3dd589f815f3
Zack Stiltner <STILTNES@email.uscb.edu>
2022-01-06 15:23:38 -0500

    changed header, custom button and homepage style sheets to style components

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -7,6 +7,8 @@ import { selectDirectorySections } from "./directory.selectors";
 
 import MenuItem from "../menu-item/menu-item.component";
 
+import './directory.styles.scss'
+
 const Directory = ({ sections }) => (
   <div className='directory-menu'> 
     {sections.map(({id, ...otherSectionProps}) => (
